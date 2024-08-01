0a79f487e00aab186186e88df92c0efd56b6ce7d
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2024-08-01 18:56:52 -0400

    I updated the schema file

--- db/schema.sql
+++ db/schema.sql
@@ -6,13 +6,14 @@ CREATE DATABASE employees_db;
 CREATE TABLE department (
   id SERIAL PRIMARY KEY,
   name VARCHAR(50) UNIQUE NOT NULL
-)
+);
 
 CREATE TABLE role (
   id SERIAL PRIMARY KEY,
   title VARCHAR(50) UNIQUE NOT NULL,
   salary DECIMAL NOT NULL,
-  department INT NOT NULL
+  department INT NOT NULL,
+  FOREIGN KEY (department) REFERENCES department(id)
 
 );
 
@@ -21,5 +22,7 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
