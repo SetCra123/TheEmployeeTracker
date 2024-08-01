5429ee0a2628f4b2c37cff2db8c38657002358e2
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2024-07-31 22:22:24 -0400

    I created my seeds file with my tables and some data to populate the tables

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+\c employees_db;
+
+INSERT INTO department (name)
+VALUES ('Photography'),
+       ('Videography'),
+       ('Sound Design'),
+       ('Audio'),
+       ('Marketing'),
+       ('Sales'),
+       ('Social Media'),
+       ('Writing'),
+       ('Management');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Director of Photography', 75000, 1),
+       ('Lighting Manager', 65000, 1),
+       ('Director', 90000, 2),
+       ('Videographer', 80000, 2),
+       ('Editor', 70000, 2),
+       ('Audio Engineer', 60000, 3),
+       ('Sound Manager', 69000, 4),
+       ('Microphone Operator', 50000, 4),
+       ('Marketing Director', 75000, 5),
+       ('Marketing Assistant', 50000, 5),
+       ('Director of Sales', 77000, 6),
+       ('Sales Assistant', 45000, 6),
+       ('Social Media Manager', 60000, 7),
+       ('Writer', 70000, 8),
+       ('CEO', 120000, 9),
