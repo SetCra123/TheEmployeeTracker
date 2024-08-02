21471eecaa75f2378176393b5b5bb3cc7107087f
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2024-08-01 22:30:58 -0400

    I added the respose (.then function) to the index.js file with the prompt

--- index.js
+++ index.js
@@ -56,13 +56,78 @@ function loadPrompts()  {
                     value: "DELETE_ROLE",
                 },
                 {
-                    
-                }
+                    name: "View All Departments",
+                    value: "VIEW_DEPARTMENTS",
+                },
+                {
+                    name: "Add New Department",
+                    value: "ADD_NEW_DEPARTMENT",
+                },
+                {
+                    name: "Delete Department",
+                    value: "DELETE_DEPARTMENT",
+                },
+                {
+                    name: "View Budget by Department",
+                    value: "VIEW_BUDGET_BY_DEPARTMENT",
+                },
+                {
+                    name: "Quit",
+                    value: "QUIT",
+                },
             
             
             
-            
-            ]
+            ],
+        },
+    ]).then((res) => {
+        let selection = res.selection;
+
+        switch (selection) {
+            case "VIEW_EMPLOYEES":
+                viewEmployees();
+                break;
+            case "VIEW_EMPLOYEES_BY_DEPARTMENT":
+                viewEmployeesByDepartment();
+                break;
+            case "ADD_NEW_EMPLOYEE":
+                addNewEmployee();
+                break;
+            case "DELETE_EMPLOYEE":
+                deleteEmployee();
+                break;
+            case "UPDATE_EMPLOYEE_ROLE":
+                updateEmployeeRole();
+                break;
+            case "VIEW_ALL_ROLES":
+                viewAllRoles();
+                break;
+            case "ADD_NEW_ROLE":
+                addNewRole();
+                break;
+            case "DELETE_ROLE":
+                deleteRole();
+                break;
+            case "VIEW_DEPARTMENTS":
+                viewDepartments();
+                break;
+            case "ADD_NEW_DEPARTMENT":
+                addNewDepartment();
+                break;
+            case "DELETE_DEPARTMENT":
+                deleteDepartment();
+                break;
+            case "VIEW_BUDGET_BY_DEPARTMENT":
+                viewBudgetbyDept();
+                break;
+            default:
+                quit();
+
         }
-    ])
+    });
+}
+
+
+function viewEmployees() {
+    db.
 }
